7c32d03562afebb6ed9aa84ef464f262f6262214
Liam <37960435+liamcco@users.noreply.github.com>
2025-05-05 13:45:20 +0200

    huge bug

--- simulation.c
+++ simulation.c
@@ -160,7 +160,7 @@ void log_execution(Processor *processor, int time)
         // Timeslot data
         if (log_timeslot_data)
         {
-            int idx = task->id + processor->m;
+            int idx = task->id + processor->m - 1;
             sim_data->timeslots[i][(time % sim_data->hyperperiod) * (sim_data->num_tasks + processor->m)+ idx]++;
         }
 
